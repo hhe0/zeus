323edab0a35647b253788a35a6173f2ce4413422
hhe0 <deng2296917779@163.com>
2019-11-01 14:46:36 +0800

    解决跨域问题

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "zeus/routers"
 )
 
 func main() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
+
 	beego.Run()
 }
--- routers/router.go
+++ routers/router.go
@@ -2,20 +2,11 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/plugins/cors"
 	controllerEmail "zeus/api/backend/email/controller"
 	"zeus/api/backend/task/controller"
 )
 
 func init() {
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowMethods:     []string{"*"},
-		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "content-type"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
-		AllowCredentials: true,
-		AllowOrigins:     []string{"http://localhost:*", "http://127.0.0.1:*"},
-	}))
-
 	// 新增任务
 	beego.Router("/api/backend/task", &controller.TaskController{}, "post:CreateTask")
 	// 任务列表
